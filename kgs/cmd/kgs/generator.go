dbb00a6447624c002a5e57c0f0579a4205c652bf
jneo8 <jneo8lin@gmail.com>
2021-02-05 15:02:44 +0800

    feat(key-generator): Finish generator tool for insert key into db

--- kgs/cmd/kgs/generator.go
+++ kgs/cmd/kgs/generator.go
@@ -9,6 +9,7 @@ import (
 	"github.com/system-design-url-shortener/kgs/pkg/progressbar"
 	"github.com/system-design-url-shortener/kgs/pkg/repository/mongo"
 	"go.uber.org/dig"
+	"math"
 	"math/rand"
 	"sync"
 	"time"
@@ -16,14 +17,13 @@ import (
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	generatorCMD.Flags().Int("n", 100000, "Number of unique key to be generated.")
-	generatorCMD.Flags().Int("batch_size", 10000, "Batch insert size.")
+	generatorCMD.Flags().Int("batch_size", 20000, "Batch insert size.")
 	generatorCMD.Flags().Int("length", 6, "Length of unique key.")
-	generatorCMD.Flags().Int("worker_n", 5, "Number of generator worker.")
+	generatorCMD.Flags().Int("worker_n", 4, "Number of generator worker.")
 	addMongoFlags(generatorCMD)
 }
 
-var base62Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+var baseLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 var generatorCMD = &cobra.Command{
 	Use:   "gen",
@@ -38,22 +38,27 @@ var generatorCMD = &cobra.Command{
 
 type generatorCMDOpts struct {
 	dig.In
-	N         int `name:"n"`
 	Length    int `name:"length"`
 	BatchSize int `name:"batch_size"`
 	WorkerN   int `name:"worker_n"`
 }
 
 func generatorRunable(logger *log.Logger, opts generatorCMDOpts, repo entity.KeyRepository) error {
+	// Close repo conn.
 	defer repo.Close()
 	if err := repo.Init(); err != nil {
+		return err
+	}
+
+	// Get worker working range.
+	workerCoverRanges, err := getWorkerCoverRanges(opts.WorkerN)
+	if err != nil {
 		return err
 	}
 
 	var wg sync.WaitGroup
 	pb, pbWg := progressbar.NewWithWaitGroup()
 	itemCh := make(chan string, opts.WorkerN)
-	total := 0
 
 	wg.Add(1)
 	go func() {
@@ -61,32 +66,45 @@ func generatorRunable(logger *log.Logger, opts generatorCMDOpts, repo entity.Key
 		keys := []string{}
 		for k := range itemCh {
 			keys = append(keys, k)
-			if len(keys) >= opts.BatchSize || total+len(keys) == opts.N {
-				numInsert, err := repo.KeyBatchInsert(keys)
+			if len(keys) >= opts.BatchSize {
+				_, err := repo.KeyBatchInsert(keys)
 				if err != nil {
 					logger.Error(err)
 				}
-				logger.Info(total)
-				total += numInsert
 				keys = keys[:0]
 			}
 		}
+		if len(keys) > 0 {
+			_, err := repo.KeyBatchInsert(keys)
+			if err != nil {
+				logger.Error(err)
+			}
+			keys = keys[:0]
+		}
 	}()
 
-	for w := 0; w <= opts.WorkerN; w++ {
+	// Generator
+	baseN := int(math.Pow(float64(len(baseLetters)), float64(opts.Length-1)))
+	for workerID, coverRange := range workerCoverRanges {
 		pbWg.Add(1)
 		wg.Add(1)
-		go func(workerID int) {
+		go func(workerID int, coverRange []int) {
 			defer pbWg.Done()
 			defer wg.Done()
-			bar := progressbar.AddBar(pb, opts.N/opts.WorkerN, fmt.Sprintf("Worker_%d", workerID))
-			for total <= opts.N {
+			bar := progressbar.AddBar(pb, (coverRange[1]+1)*baseN-coverRange[0]*baseN, fmt.Sprintf("Worker_%d", workerID))
+			for i := coverRange[0] * baseN; i < (coverRange[1]+1)*baseN; i++ {
 				start := time.Now()
-				itemCh <- randStringRunes(opts.Length)
+				n := i
+				s := ""
+				for n > 0 {
+					s = s + string(baseLetters[n%len(baseLetters)])
+					n = n / len(baseLetters)
+				}
+				itemCh <- s
 				bar.Increment()
 				bar.DecoratorEwmaUpdate(time.Since(start))
 			}
-		}(w)
+		}(workerID, coverRange)
 	}
 
 	wg.Add(1)
@@ -96,15 +114,35 @@ func generatorRunable(logger *log.Logger, opts generatorCMDOpts, repo entity.Key
 		close(itemCh)
 	}()
 	wg.Wait()
-
-	logger.Infof("Total insert %d", total)
 	return nil
+}
+
+func getWorkerCoverRanges(workerN int) ([][]int, error) {
+	if workerN > len(baseLetters) {
+		return nil, fmt.Errorf("Worker number couldn't > %d", len(baseLetters))
+	}
+
+	workerCoverRanges := [][]int{}
+	quotient := len(baseLetters) / workerN
+	for i := 0; i < workerN; i++ {
+		workerCoverRanges = append(
+			workerCoverRanges,
+			[]int{i * quotient, (i+1)*quotient - 1},
+		)
+	}
+	if len(baseLetters)%workerN != 0 {
+		workerCoverRanges = append(
+			workerCoverRanges,
+			[]int{len(baseLetters) - len(baseLetters)%workerN, len(baseLetters) - 1},
+		)
+	}
+	return workerCoverRanges, nil
 }
 
 func randStringRunes(length int) string {
 	b := make([]rune, length)
 	for i := range b {
-		b[i] = base62Letters[rand.Intn(len(base62Letters))]
+		b[i] = baseLetters[rand.Intn(len(baseLetters))]
 	}
 	return string(b)
 }
